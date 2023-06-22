0a2a890e7ca6e420d4eea3720c13628c7dc65132
Kdelphinus <kmj951015@gmail.com>
2023-06-22 14:00:27 +0900

    [tmp.py] 42, solved api 연습할 수 있는 코드 추가

--- tmp.py
+++ tmp.py
@@ -1,5 +1,35 @@
+import requests
 from intra import ic
 
-response = ic.get("users", params={"filter[login]": "myko"})
-loc = response.json()
-print(loc)
+
+def intra_api(user: str) -> None:
+    """
+    42api를 통해 유저 정보를 출력하는 함수
+    Args:
+        user: intra id
+    """
+    response = ic.get("users", params={"filter[login]": user})
+    try:
+        loc = response.json()
+        print(loc)
+    except:
+        print(response)
+
+
+def solved_api(user: str) -> None:
+    """
+    solved api를 통해 유저 정보를 출력하는 함수
+    Args:
+        user: baekjoon id
+    """
+    url = "https://solved.ac/api/v3/user/show"
+    querystring = {"handle": user}
+    headers = {"Accept": "application/json"}
+    response = requests.get(url, headers=headers, params=querystring)
+    print(response)
+    print(response.json())
+
+
+if __name__ == "__main__":
+    intra_api("myko")
+    solved_api("kmj951015")
