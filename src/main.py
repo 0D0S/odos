f06c2d5d9b7ebe4cff087fe9d88fe16469969e66
Myoung Jun Ko <kmj951015@gmail.com>
2023-07-24 18:44:53 +0900

    :ambulance: tab -> space 4

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import odos
 if __name__ == "__main__":
     odos.read_and_write_csv()
     text = odos.print_result()
-		if input("독촉방에 올리시겠습니까?(y/n)") == "y":
-	    odos.chat_message("독촉", text)
-		else:
-	    odos.chat_message("odos", text)
+    if input("독촉방에 올리시겠습니까?(y/n)") == "y":
+        odos.chat_message("독촉", text)
+    else:
+        odos.chat_message("test", text)
