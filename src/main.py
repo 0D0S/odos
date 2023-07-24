99403fd960306098e7253c953508752fb25a924a
Myoung Jun Ko <kmj951015@gmail.com>
2023-07-24 18:21:16 +0900

    :white_check_mark: maina문에 test 추가

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ import odos
 if __name__ == "__main__":
     odos.read_and_write_csv()
     text = odos.print_result()
-    odos.chat_message("독촉", text)
+    # odos.chat_message("독촉", text)
+    odos.chat_message("odos", text)
