3225dbcb2b69beea4b7f6da196c37bd8f75eea2e
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-16 08:11:12 +0900

    :fire: test 제거
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- src/main.py
+++ src/main.py
@@ -3,7 +3,4 @@ import odos
 if __name__ == "__main__":
     odos.read_and_write_csv()
     text = odos.print_result()
-    if input("독촉방에 올리시겠습니까?(y/n)") == "y":
-        odos.chat_message("독촉", text)
-    else:
-        odos.chat_message("test", text)
+    odos.chat_message("독촉", text)
