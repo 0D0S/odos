b1631382b6a06812f7eabf73a5cf9f3ecb391953
Kdelphinus <kmj951015@gmail.com>
2023-11-17 18:48:01 +0900

    :memo: update 2023-11-17+18:48

--- git_autofication.sh
+++ git_autofication.sh
@@ -8,9 +8,10 @@ TOKEN=$env_github_token
 REPO_NAME="odos"
 
 # 로컬에서 작업한 변경사항을 커밋
+cd /home/ubuntu/odos
 git pull
-git add .
+git add . 
 git commit -m ":memo: update `date +%F+%H:%M`"
 
 # 조직 이름과 토큰을 사용하여 GitHub에 push
-git push https://$ORG_NAME:$TOKEN@github.com/$ORG_NAME/$REPO_NAME.git
+git push https://$ORG_NAME:$TOKEN@github.com/$ORG_NAME/$REPO_NAME.git main
