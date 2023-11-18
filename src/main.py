053e5c0d6132dff1130a34a4d804166eed52e2df
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-18 21:07:39 +0900

    :ambulance: flag => is_last_check로 수정
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import datetime
 import odos
 
 if __name__ == "__main__":
-    flag = datetime.datetime.now().hour < 6
-    odos.read_and_write_csv(flag)
-    if not flag:
+    is_last_check = datetime.datetime.now().hour < 6
+    odos.read_and_write_csv(is_last_check)
+    if not is_last_check:
         text = odos.print_result()
         odos.chat_message("독촉", text)
